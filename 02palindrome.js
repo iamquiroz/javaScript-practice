c6cdfbe6f5c2cf894e0909115c94fb60436508aa
Josh W <sdkjqg@gmail.com>
2022-02-15 20:09:40 -0800

    solution 2

--- 02palindrome.js
+++ 02palindrome.js
@@ -1,10 +1,9 @@
-
 function hasWhiteSpace(s) {
   return /\s/.test(s);
 }
 
-function solution (str) {
-  return str == str.split('').reverse().join('');
+function solution(str) {
+  return str == str.split("").reverse().join("");
 }
 
 /*
@@ -15,10 +14,33 @@ function solution (str) {
 	The one posted by Jason Sebring in this thread is both simple and efficient (time O(n), space O(1)).
 
 */
-//console.log(hasWhiteSpace("hello World!"));
-//console.log(hasWhiteSpace("HelloWorld!"));
+// CheckPalindrome Solution 2
 
-console.log(solution("Hello World"));
-console.log(solution("anita lava la tina"));
-console.log(solution("abcd"));
-console.log(solution("aabaa"));
+function solution2(inputString) {
+  // finde the length of a string
+  const length = inputString.length;
+
+  // loop through half of the string
+  for (let i = 0; i < length / 2; i++) {
+    // check if first and last string are same
+    if (inputString[i] !== inputString[length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+let string1 = "Hello World";
+let string2 = "anita lava la tina";
+let string3 = "abcd";
+let string4 = "aabaa";
+
+console.log(solution2(string1));
+console.log(solution2(string2));
+console.log(solution2(string3));
+console.log(solution2(string4));
+
+// missing map implementation
+// better solution for palindrome
+// how to implement testing
+
