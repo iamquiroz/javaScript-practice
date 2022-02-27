01bbae3e46b11a19a256530850b73376c976c6cf
Josh W <sdkjqg@gmail.com>
2022-02-27 12:45:59 -0800

    some files deleted

---
+++ 03findWord.js
@@ -0,0 +1,16 @@
+
+
+
+let word = 'casa';
+let book='En la con casa de mi tia';
+
+
+function findWord(book,word){
+  let auxBook = book.split(' ');
+  return auxBook;
+}
+
+
+console.log(findWord(book, word));
+
+
