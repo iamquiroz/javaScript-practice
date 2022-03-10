d8eb18e522ebbc747d301cfb6102d120adaf54b7
Josh W <sdkjqg@gmail.com>
2022-03-10 14:01:22 -0800

    links.js added or modified

---
+++ link-tree-app-nextjs/pages/links.js
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <ul class="content-block__list unordered">
+      <li class="mb-2">
+        <span
+          ><a href="https://www.hackerrank.com/" target="_blank"
+            >HackerRank</a
+          ></span
+        >
+      </li>
+      <li class="mb-2">
+        <span
+          ><a href="https://leetcode.com/" target="_blank">Leetcode</a></span
+        >
+      </li>
+      <li class="mb-2">
+        <span
+          ><a href="https://exercism.org/" target="_blank">Exercism</a></span
+        >
+      </li>
+      <li class="mb-2">
+        <span
+          ><a href="https://www.pramp.com/#/" target="_blank">Pramp</a></span
+        >
+      </li>
+      <li class="mb-2">
+        <span
+          ><a href="https://www.hackerearth.com/" target="_blank"
+            >HackerEarth</a
+          ></span
+        >
+      </li>
+      <li class="mb-2">
+        <span
+          ><a href="https://www.qualified.io/" target="_blank"
+            >Qualified.io</a
+          ></span
+        >
+      </li>
+      <li class="mb-2">
+        <span
+          ><a href="https://codesignal.com/" target="_blank"
+            >Codesignal</a
+          ></span
+        >
+      </li>
+    </ul>
+  </body>
+
+  <style >
+    tab-size {4};
+    -webkit-text-size-adjust: 100%;
+    font-family: inherit;
+    line-height: inherit;
+    color: #424770;
+    --tw-bg-opacity: 1;
+    font-size: 16px;
+    box-sizing: border-box;
+    border-width: 0;
+    border-style: solid;
+    --tw-border-opacity: 1;
+    border-color: rgba(237, 242, 247, var(--tw-border-opacity));
+    --tw-shadow: 0 0 #0000;
+    --tw-ring-inset: var(--tw-empty,/*!*/ /*!*/);
+    --tw-ring-offset-width: 0px;
+    --tw-ring-offset-color: #fff;
+    --tw-ring-color: rgba(59, 130, 246, 0.5);
+    --tw-ring-offset-shadow: 0 0 #0000;
+    --tw-ring-shadow: 0 0 #0000;
+    margin: 0;
+    padding: 0;
+    padding-left: 21px;
+    list-style: disc;
+
+    tab-size: 4;
+    -webkit-text-size-adjust: 100%;
+    font-family: inherit;
+    line-height: inherit;
+    color: #424770;
+    --tw-bg-opacity: 1;
+    font-size: 16px;
+    box-sizing: border-box;
+    border-width: 0;
+    border-style: solid;
+    --tw-border-opacity: 1;
+    border-color: rgba(237, 242, 247, var(--tw-border-opacity));
+    --tw-shadow: 0 0 #0000;
+    --tw-ring-inset: var(--tw-empty,/*!*/ /*!*/);
+    --tw-ring-offset-width: 0px;
+    --tw-ring-offset-color: #fff;
+    --tw-ring-color: rgba(59, 130, 246, 0.5);
+    --tw-ring-offset-shadow: 0 0 #0000;
+    --tw-ring-shadow: 0 0 #0000;
+    margin: 0;
+    padding: 0;
+    padding-left: 21px;
+    list-style: disc;
+    
+  </style>
+  
+</html>
